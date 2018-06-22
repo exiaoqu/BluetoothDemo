b69fbfd9d5821ba5f163e7a7719c973b460a52bb
Bin Fan <bin.fan@ericsson.com>
2018-06-22 20:17:51 +0800

    modify TtsModule

--- app/src/main/java/com/compass/tts/TtsModule.java
+++ app/src/main/java/com/compass/tts/TtsModule.java
@@ -1,44 +1,14 @@
 package com.compass.tts;
 
-//import android.Manifest;
 import android.content.Context;
-//import android.content.pm.PackageManager;
-//import android.os.Bundle;
-//import android.support.v4.app.ActivityCompat;
-//import android.support.v4.content.ContextCompat;
-//import android.support.v7.app.AppCompatActivity;
-//import android.os.Bundle;
-//import android.Manifest;
-//import android.content.pm.PackageManager;
-//import android.media.AudioManager;
-//import android.os.Handler;
-//import android.os.Message;
-//import android.support.v4.app.ActivityCompat;
-//import android.support.v4.content.ContextCompat;
-import android.os.Environment;
 import android.util.Log;
-//import android.view.View;
-//import android.widget.Button;
-//import android.widget.TextView;
-//import com.baidu.duer.dcs.R;
 import com.baidu.tts.auth.AuthInfo;
 import com.baidu.tts.chainofresponsibility.logger.LoggerProxy;
 import com.baidu.tts.client.SpeechError;
 import com.baidu.tts.client.SpeechSynthesizer;
 import com.baidu.tts.client.SpeechSynthesizerListener;
 import com.baidu.tts.client.TtsMode;
-
-//import com.compass.tts.control.InitConfig;
-//import com.compass.tts.listener.UiMessageListener;
-//import com.compass.tts.util.AutoCheck;
-
 import java.io.File;
-//import java.util.ArrayList;
-//import java.util.ArrayList;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
-import java.io.IOException;
-import java.io.InputStream;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -52,65 +22,36 @@ public class TtsModule implements SpeechSynthesizerListener{
     private static final String TAG = "TtsModule";
 
     // ================== 初始化参数设置开始 ==========================
-    private String appId = "11189849";//请更换为自己创建的应用
-    private static final String appKey = "yoLqWgEbzNgKKprVT7iSh9YK";//请更换为自己创建的应用
-    private static final String secretKey = "cf7fb720d813411714ec85a5429ea75f";
+    protected String appId = "11189849";
+    protected String appKey = "yoLqWgEbzNgKKprVT7iSh9YK";
+    protected String secretKey = "cf7fb720d813411714ec85a5429ea75f";
+
     // TtsMode.MIX; 离在线融合，在线优先； TtsMode.ONLINE 纯在线； 没有纯离线
     private TtsMode ttsMode = TtsMode.ONLINE;
 
-    private String mSampleDirPath;
-    private static final String SAMPLE_DIR_NAME = "baiduTTS";
-    private static final String SPEECH_FEMALE_MODEL_NAME = "bd_etts_common_speech_f7_mand_eng_high_am-mix_v3.0.0_20170512.dat";
-    private static final String SPEECH_YYJW_MODEL_NAME = "bd_etts_common_speech_yyjw_mand_eng_high_am-mix_v3.0.0_20170512.dat";
-    private static final String TEXT_MODEL_NAME = "bd_etts_text.dat";
+    // ================选择TtsMode.ONLINE  不需要设置以下参数; 选择TtsMode.MIX 需要设置下面2个离线资源文件的路径
+    private static final String TEMP_DIR = "/sdcard/baiduTTS"; // 重要！请手动将assets目录下的3个dat 文件复制到该目录
 
-    private String MODEL_FILENAME;
-    private String TEXT_FILENAME;
-    private String YYJW_MODEL_NAME;
+    // 请确保该PATH下有这个文件
+    private static final String TEXT_FILENAME = TEMP_DIR + "/" + "bd_etts_text.dat";
 
-    private Context context;
+    // 请确保该PATH下有这个文件 ，m15是离线男声
+    private static final String MODEL_FILENAME = TEMP_DIR + "/" + "bd_etts_common_speech_as_mand_eng_high_am_v3.0.0_20170516.dat";
 
     // ===============初始化参数设置完毕，更多合成参数请至getParams()方法中设置 =================
-
-    private SpeechSynthesizer mSpeechSynthesizer=null;
-
-    private static final String DESC = "精简版合成，仅给出示例集成合成的调用过程。可以测试离线合成功能，首次使用请联网。\n"
-            + "其中initTTS方法需要在新线程调用，否则引起UI阻塞。\n"
-            + "纯在线请修改代码里ttsMode为TtsMode.ONLINE， 没有纯离线。\n"
-            + "离线功能需要手动将assets目录下的资源文件复制到TEMP_DIR =/baiduTTS \n"
-            + "完整的SDK调用方式可以参见MainActivity\n\n";
+    protected SpeechSynthesizer mSpeechSynthesizer;
 
     /**
-     * 定义一个静态私有变量(不初始化，不使用final关键字，
-     * 使用volatile保证了多线程访问时instance变量的可见性，
-     * 避免了instance初始化时其他变量属性还没赋值完时，被另外线程调用
+     * 单例模式
+     *
      * */
     private static volatile TtsModule instance;
+    private Context context;
 
-    private TtsModule(Context context)
-    {
+    private TtsModule(Context context){
         this.context = context;
         initTTs();
     }
-
-    /**
-     * 采用单例模式,内存占用地，效率高，线程安全，多线程操作原子性。
-     * @param1 context为上下文环境
-     * @return MixSpeakUtil实例
-     *
-     * */
-//    public static TtsModule getInstance(Context context) {
-//        if (instance == null) {
-//            //同步代码块（对象未初始化时，使用同步代码块，保证多线程访问时对象在第一次创建后，不再重复被创建）
-//            synchronized (TtsModule.class) {
-//                //未初始化，则初始instance变量
-//                if (instance == null) {
-//                    instance = new TtsModule(context);
-//                }
-//            }
-//        }
-//        return instance;
-//    }
     public static void initializeInstance(Context context) {
         if (null == instance) {
             //同步代码块（对象未初始化时，使用同步代码块，保证多线程访问时对象在第一次创建后，不再重复被创建）
@@ -125,6 +66,10 @@ public class TtsModule implements SpeechSynthesizerListener{
         return instance;
     }
 
+    /**
+     * 注意此处为了说明流程，故意在UI线程中调用。
+     * 实际集成中，该方法一定在新线程中调用，并且该线程不能结束。具体可以参考NonBlockSyntherizer的写法
+     */
     private void initTTs() {
         LoggerProxy.printable(true); // 日志打印在logcat中
         boolean isMix = ttsMode.equals(TtsMode.MIX);
@@ -135,15 +80,19 @@ public class TtsModule implements SpeechSynthesizerListener{
             if (!isSuccess) {
                 return;
             } else {
-                print("离线资源存在并且可读, 目录：" + mSampleDirPath);
+                Log.i(TAG,"离线资源存在并且可读, 目录：" + TEMP_DIR);
             }
         }
 
+//        // 日志更新在UI中，可以换成MessageListener，在logcat中查看日志
+//        SpeechSynthesizerListener listener = new UiMessageListener(mainHandler);
 
         // 1. 获取实例
         mSpeechSynthesizer = SpeechSynthesizer.getInstance();
-        mSpeechSynthesizer.setContext(context);
+        mSpeechSynthesizer.setContext(this.context);
 
+//        // 2. 设置listener
+//        mSpeechSynthesizer.setSpeechSynthesizerListener(listener);
 
         // 3. 设置appId，appKey.secretKey
         int result = mSpeechSynthesizer.setAppId(appId);
@@ -168,7 +117,7 @@ public class TtsModule implements SpeechSynthesizerListener{
         // 设置在线发声音人： 0 普通女声（默认） 1 普通男声 2 特别男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>
         mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEAKER, "0");
         // 设置合成的音量，0-9 ，默认 5
-        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_VOLUME, "9");
+        mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_VOLUME, "5");
         // 设置合成的语速，0-9 ，默认 5
         mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEED, "5");
         // 设置合成的语调，0-9 ，默认 5
@@ -181,7 +130,7 @@ public class TtsModule implements SpeechSynthesizerListener{
         // MIX_MODE_HIGH_SPEED_NETWORK ， 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
         // MIX_MODE_HIGH_SPEED_SYNTHESIZE, 2G 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
 
-        //mSpeechSynthesizer.setAudioStreamType(AudioManager.MODE_IN_CALL);
+//        mSpeechSynthesizer.setAudioStreamType(AudioManager.MODE_IN_CALL);
 
         // x. 额外 ： 自动so文件是否复制正确及上面设置的参数
         Map<String, String> params = new HashMap<>();
@@ -192,12 +141,14 @@ public class TtsModule implements SpeechSynthesizerListener{
             params.put(SpeechSynthesizer.PARAM_TTS_SPEECH_MODEL_FILE, MODEL_FILENAME);
         }
 
-
         // 6. 初始化
         result = mSpeechSynthesizer.initTts(ttsMode);
         checkResult(result, "initTts");
 
+        //7. 初始化显示
+        speak(TEXT);
     }
+
 
     /**
      * 检查appId ak sk 是否填写正确，另外检查官网应用内设置的包名是否与运行时的包名一致。本demo的包名定义在build.gradle文件中
@@ -209,10 +160,10 @@ public class TtsModule implements SpeechSynthesizerListener{
         if (!authInfo.isSuccess()) {
             // 离线授权需要网站上的应用填写包名。本demo的包名是com.baidu.tts.sample，定义在build.gradle中
             String errorMsg = authInfo.getTtsError().getDetailMessage();
-            print("【error】鉴权失败 errorMsg=" + errorMsg);
+            Log.i(TAG,"【error】鉴权失败 errorMsg=" + errorMsg);
             return false;
         } else {
-            print("验证通过，离线正式授权文件存在。");
+            Log.i(TAG,"验证通过，离线正式授权文件存在。");
             return true;
         }
     }
@@ -223,63 +174,32 @@ public class TtsModule implements SpeechSynthesizerListener{
      * @return
      */
     private boolean checkOfflineResources() {
-        if (mSampleDirPath == null) {
-            String sdcardPath = Environment.getExternalStorageDirectory().toString();
-            mSampleDirPath = sdcardPath + "/" + SAMPLE_DIR_NAME;
-        }
-        File file = new File(mSampleDirPath);
-        if (!file.exists()) {
-            file.mkdirs();
-        }
-        TEXT_FILENAME = mSampleDirPath + "/" + TEXT_MODEL_NAME;
-        MODEL_FILENAME = mSampleDirPath + "/" + SPEECH_FEMALE_MODEL_NAME;
-        YYJW_MODEL_NAME = mSampleDirPath + "/" + SPEECH_YYJW_MODEL_NAME;
-        copyFromAssetsToSdcard(false, SPEECH_FEMALE_MODEL_NAME, MODEL_FILENAME);
-        copyFromAssetsToSdcard(false, SPEECH_YYJW_MODEL_NAME, YYJW_MODEL_NAME);
-        copyFromAssetsToSdcard(false, TEXT_MODEL_NAME, TEXT_FILENAME);
         String[] filenames = {TEXT_FILENAME, MODEL_FILENAME};
         for (String path : filenames) {
             File f = new File(path);
             if (!f.canRead()) {
-                print("[ERROR] 文件不存在或者不可读取，请从assets目录复制同名文件到：" + path);
-                print("[ERROR] 初始化失败！！！");
+                Log.i(TAG,"[ERROR] 文件不存在或者不可读取，请从assets目录复制同名文件到：" + path);
+                Log.i(TAG,"[ERROR] 初始化失败！！！");
                 return false;
             }
         }
         return true;
     }
 
-    public void speak(String msg) {
-        /* 以下参数每次合成时都可以修改
-         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEAKER, "0");
-         *  设置在线发声音人： 0 普通女声（默认） 1 普通男声 2 特别男声 3 情感男声<度逍遥> 4 情感儿童声<度丫丫>
-         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_VOLUME, "5"); 设置合成的音量，0-9 ，默认 5
-         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_SPEED, "5"); 设置合成的语速，0-9 ，默认 5
-         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_PITCH, "5"); 设置合成的语调，0-9 ，默认 5
-         *
-         *  mSpeechSynthesizer.setParam(SpeechSynthesizer.PARAM_MIX_MODE, SpeechSynthesizer.MIX_MODE_DEFAULT);
-         *  MIX_MODE_DEFAULT 默认 ，wifi状态下使用在线，非wifi离线。在线状态下，请求超时6s自动转离线
-         *  MIX_MODE_HIGH_SPEED_SYNTHESIZE_WIFI wifi状态下使用在线，非wifi离线。在线状态下， 请求超时1.2s自动转离线
-         *  MIX_MODE_HIGH_SPEED_NETWORK ， 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
-         *  MIX_MODE_HIGH_SPEED_SYNTHESIZE, 2G 3G 4G wifi状态下使用在线，其它状态离线。在线状态下，请求超时1.2s自动转离线
-         */
-
+    public  void speak(String text) {
         if (mSpeechSynthesizer == null) {
-            print("[ERROR], 初始化失败");
+            Log.i("ERROR", "初始化失败!");
             return;
         }
-        int result = mSpeechSynthesizer.speak(msg);
-        print("合成并播放");
+        int result = mSpeechSynthesizer.speak(text);
+        Log.i(TAG, "合成成功！");
         checkResult(result, "speak");
     }
 
-    public void stop() {
-        print("停止合成引擎 按钮已经点击");
-        int result = mSpeechSynthesizer.stop();
-        checkResult(result, "stop");
-    }
-    private void print(String message) {
-        Log.i(TAG, message);
+    private void checkResult(int result, String method) {
+        if (result != 0) {
+            Log.i(TAG,"error code :" + result + " method:" + method + ", 错误码文档:http://yuyin.baidu.com/docs/tts/122 ");
+        }
     }
 
     protected void onDestroy() {
@@ -287,75 +207,41 @@ public class TtsModule implements SpeechSynthesizerListener{
             mSpeechSynthesizer.stop();
             mSpeechSynthesizer.release();
             mSpeechSynthesizer = null;
-            print("释放资源成功");
         }
     }
 
-    private void checkResult(int result, String method) {
-        if (result != 0) {
-            print("error code :" + result + " method:" + method + ", 错误码文档:http://yuyin.baidu.com/docs/tts/122 ");
-        }
-    }
-
-
-    /**
-     * 将工程需要的资源文件拷贝到SD卡中使用（授权文件为临时授权文件，请注册正式授权）
-     *
-     * @param isCover 是否覆盖已存在的目标文件
-     * @param source
-     * @param dest
-     */
-    private void copyFromAssetsToSdcard(boolean isCover, String source, String dest) {
-        File file = new File(dest);
-        if (isCover || (!isCover && !file.exists())) {
-            InputStream is = null;
-            FileOutputStream fos = null;
-            try {
-                is = context.getAssets().open(source);
-                String path = dest;
-                fos = new FileOutputStream(path);
-                byte[] buffer = new byte[1024];
-                int size = 0;
-                while ((size = is.read(buffer, 0, 1024)) >= 0) {
-                    fos.write(buffer, 0, size);
-                }
-            } catch (FileNotFoundException e) {
-                e.printStackTrace();
-            } catch (IOException e) {
-                e.printStackTrace();
-            } finally {
-                if (fos != null) {
-                    try {
-                        fos.close();
-                    } catch (IOException e) {
-                        e.printStackTrace();
-                    }
-                }
-                try {
-                    if (is != null) {
-                        is.close();
-                    }
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
-            }
-        }
+    @Override
+    public void onSynthesizeStart(String s) {
 
     }
 
+    @Override
+    public void onSynthesizeDataArrived(String s, byte[] bytes, int i) {
+
+    }
 
     @Override
-    public void onSynthesizeStart(String s) {}
+    public void onSynthesizeFinish(String s) {
+
+    }
+
     @Override
-    public void onSynthesizeDataArrived(String s, byte[] bytes, int i) {}
+    public void onSpeechStart(String s) {
+
+    }
+
     @Override
-    public void onSynthesizeFinish(String s) {}
+    public void onSpeechProgressChanged(String s, int i) {
+
+    }
+
     @Override
-    public void onSpeechStart(String s) {}
+    public void onSpeechFinish(String s) {
+
+    }
+
     @Override
-    public void onSpeechProgressChanged(String s, int i) {}
-    @Override
-    public void onSpeechFinish(String s) {}
-    @Override
-    public void onError(String s, SpeechError speechError) {}
+    public void onError(String s, SpeechError speechError) {
+
+    }
 }
